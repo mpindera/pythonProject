1806ddfedd24e2f1eaf3940e2305bbef53fce00b
mpindera <mikolaj.pindera@op.pl>
2023-05-31 12:47:07 +0200

    update

--- py/sendMail.py
+++ py/sendMail.py
@@ -1,32 +1,63 @@
-from tkinter import *
-from tkinter import filedialog
+import tkinter as tk
+from tkinter import messagebox
+from tkinter.scrolledtext import ScrolledText
+import smtplib
+from email.message import EmailMessage
+import ssl
+from tkinter import IntVar
 
-def open_file():
-    filepath = filedialog.askopenfilename(filetypes=[("patient_emails", "*.txt")])
-    if filepath:
-        with open(filepath, "r") as file:
-            content = file.read()
-            text.delete("1.0", "end")
-            text.insert("1.0", content)
 
-def select_text():
-    selected_text = text.get("sel.first", "sel.last")
-    print("Selected Text:", selected_text)
+def send_email(sender_email, password, selected_emails, subject, message):
+    context = ssl.create_default_context()
+    for email in selected_emails:
+        try:
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+                smtp.login(sender_email, password)
+                msg = EmailMessage()
+                msg['From'] = sender_email
+                msg['To'] = email
+                msg['Subject'] = subject
+                msg.set_content(message)
+                smtp.send_message(msg)
+            messagebox.showinfo('Email', "Wiadomość została wysłana.")
+        except Exception as e:
+            messagebox.showerror('Błąd', f"Wystąpił błąd podczas wysyłania wiadomości: {str(e)}")
 
-root = Tk()
+def read_emails(filename):
+    with open(filename, 'r') as file:
+        emails = file.read().split('|')
+    return [email.strip() for email in emails if email.strip()]
 
-scrollbar = Scrollbar(root)
-scrollbar.pack(side=RIGHT, fill=Y)
+def send_message():
+    sender_email = 'mikolajpindera2@gmail.com'
+    password = 'wwalnnbrcohqkgvk'
+    subject = 'Subject of the email'
+    message = 'Body of the email'
 
-text = Text(root, yscrollcommand=scrollbar.set)
-text.pack(side=LEFT, fill=BOTH)
+    selected_emails = [email for email, checkbox_var in checkbox_vars.items() if checkbox_var.get() == 1]
 
-scrollbar.config(command=text.yview)
+    if selected_emails:
+        send_email(sender_email, password, selected_emails, subject, message)
+    else:
+        messagebox.showwarning('Brak zaznaczenia', 'Proszę zaznaczyć co najmniej jeden email.')
 
-open_button = Button(root, text="Open File", command=open_file)
-open_button.pack()
 
-select_button = Button(root, text="Select Text", command=select_text)
-select_button.pack()
+def create_checkbox_list(root, emails):
+    checkbox_vars = {}
+    for email in emails:
+        checkbox_var = IntVar()
+        checkbox = tk.Checkbutton(root, text=email, variable=checkbox_var)
+        checkbox.pack(anchor="w")
+        checkbox_vars[email] = checkbox_var
+
+    return checkbox_vars
+
+root = tk.Tk()
+
+emails = read_emails('patient_emails.txt')
+checkbox_vars = create_checkbox_list(root, emails)
+
+send_button = tk.Button(root, text="Wyślij", command=send_message)
+send_button.pack()
 
 root.mainloop()
